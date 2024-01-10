492d760cf1151d2b33228a2643e27866dc336f10
Rajesh Kolla <razesh.kolla@gmail.com>
2024-01-10 01:58:41 -0500

    added Test sample project for dotnet utility

---
+++ Samples/1.OverviewOfDotnetUtility/TestSolution/DTO.Interfaces/IPerson.cs
@@ -0,0 +1,22 @@
+﻿/// <summary>
+/// Represents a person.
+/// </summary>
+namespace DTO.Interfaces
+{
+    /// <summary>
+    /// Represents a person.
+    /// </summary>
+    public interface IPerson
+    {
+        /// <summary>
+        /// Gets or sets the first name of the person.
+        /// </summary>
+        string FirstName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the last name of the person.
+        /// </summary>
+        string LastName { get; set; }
+    }
+}
+
